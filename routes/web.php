62b6eefc1306f370ace8bcf54537c2f6957f5853
nicol <nicoln042001@gmail.com>
2025-06-11 23:24:27 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\HabitacionController;
 
+Route::get('/', function () {
+    return '¡Bienvenido a la API de Hoteles!';
+});
+
 Route::controller(HotelController::class)->prefix('hoteles')->group(function () {
     Route::get('/', 'index')->name('hoteles.index');
     Route::get('/{id}/edit', 'edit')->name('hoteles.edit');
